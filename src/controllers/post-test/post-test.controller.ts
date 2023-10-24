333ab9153b743ca2d0a5de5f1f8bbddc435b7283
Anton <famtripuae19@gmail.com>
2023-10-24 14:47:16 +0300

    feat: add typescript to the project

---
+++ src/controllers/post-test/post-test.controller.ts
@@ -0,0 +1,19 @@
+import { type Request, type Response } from 'express'
+import { HttpError } from '../../errors/http-error'
+import { ApiErrors } from '../../enums/API-errors.enum'
+
+
+const getPostTestResponse = (req: Request, res: Response): void => {
+    const requestText: string | undefined = req.body.text
+
+    if (requestText === undefined || requestText === null) {
+        throw new HttpError(
+            ApiErrors.BAD_REQUEST,
+            400
+        )
+    }
+
+    res.status(200).json({ data: requestText })
+}
+
+export { getPostTestResponse }
